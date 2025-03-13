b08f5b0d20f15e5a6e8e0a2250c64a6f9e1d8614
Yoshiki-tt <tokyometal0529@yahoo.co.jp>
2025-03-13 16:18:49 +0900

    Create CarExec_Chapter15.java

---
+++ CarExec_Chapter15.java
@@ -0,0 +1,12 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+   public static void main(String[]  args) {
+	   Car_Chapter15 X3 = new Car_Chapter15();
+	   
+	   X3.gearChange(3);
+	   X3.run();
+	   
+			   
+   }
+}
